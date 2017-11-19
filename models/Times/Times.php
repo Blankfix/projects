3fdd8fa865688fd910f44ee968ecdbd5ce91caec
cyril.deconinck <cyril.deconinck@gmail.com>
2017-11-19 19:22:20 +0100

    Merge branch 'master' of https://github.com/Blankfix/projects
    # Conflicts:
    #	controllers/User/ListController.php
    #	controllers/User/LoginController.php

--- models/Times/Times.php
+++ models/Times/Times.php
@@ -1,5 +1,23 @@
 <?php
-    $request = $sql->query('SELECT * from project');
-    while ($data = $results->fetch()){
-        echo $data['title'];
+require_once('../../models/inc/database.php');
+
+class Times{
+    public function getAllTimes(){
+        $db = new Database();
+        $data = $db->query('SELECT * FROM project');
+
+        return $data;
     }
+    public function getUserTimes(){
+        $db = new Database();
+        $data = $db->query('SELECT * FROM project where TODO');
+
+        return $data;
+    }
+}
+
+$times = new Times();
+$getTimes = $times->getAllTimes();
+while ($result = $getTimes->fetchAll(PDO::FETCH_OBJ)){
+    var_dump($result);
+}
