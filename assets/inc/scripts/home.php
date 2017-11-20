10a37003f5611276110fb2901291f8b65b5b87eb
Adèle ROYER <aroyer@datasolution.fr>
2017-11-20 09:38:19 +0100

    Ajout popper.js + fonticon Materials + Guide de style (en cours) + ébauche home en cours

--- assets/inc/scripts/home.php
+++ assets/inc/scripts/home.php
@@ -2,4 +2,5 @@
 <script src="assets/libs/jquery/jquery-3.2.1.min.js"></script>
 
 <!-- Bootstrap -->
+<script src="assets/libs/popper/js/popper.min.js"></script>
 <script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
--- assets/inc/styles/home.php
+++ assets/inc/styles/home.php
@@ -1,5 +1,18 @@
 <!-- CSS :: Home -->
 
-<!-- Bootstrap -->
+<!-- VENDOR -->
+
+<!-- VENDOR :: Bootstrap -->
 <link rel="stylesheet" type="text/css" href="assets/libs/bootstrap/css/bootstrap.min.css">
+<!-- VENDOR :: Font-awesome -->
+<link rel="stylesheet" type="text/css" href="assets/libs/font-awesome/css/font-awesome.min.css">
+<!-- VENDOR :: Material Icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- APP -->
+<link rel="stylesheet" type="text/css" href="assets/css/theme/main.css">
+
+<!-- APP :: Override -->
+<link rel="stylesheet" type="text/css" href="assets/css/override/bootstrap-override.css">
+
 
